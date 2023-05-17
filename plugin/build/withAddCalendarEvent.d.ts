d42cb7822bb4f6533ab75d0492d1fb3a348a22ed
Michiel van Roon <michiel@michielvanroon.nl>
2023-05-17 08:46:36 +0200

    remove build folder

--- plugin/build/withAddCalendarEvent.d.ts
+++
@@ -1,7 +0,0 @@
-import { ConfigPlugin } from "expo/config-plugins";
-interface ConfigPluginProps {
-    calendarPermission?: string;
-    contactsPermission?: string;
-}
-declare const withAddCalendarEvent: ConfigPlugin<void | ConfigPluginProps>;
-export default withAddCalendarEvent;
